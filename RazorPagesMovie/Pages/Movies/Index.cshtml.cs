1a69fc479d37db23498bb1fbb67409c1b195c7ea
minh217 <hunter2171996@gmail.com>
2024-01-12 17:12:34 +0700

    update project

---
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using RazorPagesMovie.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorPagesMovie.Pages_Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString {get;set;}
+        public SelectList? Genres {get;set;}
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre {get;set;}
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                        select m;
+            if(!string.IsNullOrWhiteSpace(SearchString))
+            {
+                movies = movies.Where(el => el.Title.Contains(SearchString));
+            }
+
+            if(!string.IsNullOrWhiteSpace(MovieGenre))
+            {
+                movies = movies.Where(el => el.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
 
 var app = builder.Build();
 
+using(var scope  = app.Services.CreateAsyncScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
